6556f191b124129f385328704ab1e2b4b19bf1f1
Naranise <165276200+Naranise@users.noreply.github.com>
2024-09-07 19:21:28 +0200

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,20 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario recargue la página
+
+    // Recoge los valores del formulario
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    // Enviar el correo usando EmailJS
+    emailjs.send('service_bwodqdf', 'template_id', {
+        from_name: nombre,
+        reply_to: email,
+        message: mensaje
+    })
+    .then(function(response) {
+        alert('Mensaje enviado con éxito!');
+    }, function(error) {
+        alert('Error al enviar el mensaje, por favor intenta de nuevo.');
+    });
+});
